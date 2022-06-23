9daf35aada15efac9cbb9e655dc11e405c47fb5b
oyku.ors <oyku.ors@univera.com.tr>
2022-06-23 23:17:08 +0300

    404 page css edited

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React from "react";
+import './style.css';
 
 const NotFound = () => {
-    return(<div><h1>404 NOT FOUND</h1></div>)
-
+  return (
+    <div class="d-flex justify-content-center align-items-center" id="main">
+      <h1 class="mr-3 pr-3 align-top border-right inline-block align-content-center">
+        404
+      </h1>
+      <div class="inline-block align-middle">
+        <h1 class="font-weight-normal lead" id="desc">
+          The page you requested was not found.
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
